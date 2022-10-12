57370a091dfd0b25c10d41af4672f225e023b5c8
mnishimori <mnishimori@yahoo.com.br>
2022-10-12 18:08:43 -0300

    Bean validation e RestControllerAdvice com exception handler

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,13 +1,24 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 	
+	@NotEmpty(message = "Informe o título")
+	@Length(min = 5)
 	private String titulo;
+	
+	@NotEmpty(message = "Informe a mensagem")
+	@Length(min = 5)
 	private String mensagem;
+	
+	@NotEmpty(message = "Informe o nome do curso")
 	private String nomeCurso;
 	
 	public String getTitulo() {
