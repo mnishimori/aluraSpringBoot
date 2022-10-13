03cb0ea855828bf6c49bbad857169862bc6897ac
mnishimori <mnishimori@yahoo.com.br>
2022-10-13 14:53:21 -0300

    Método paginado para pesquisa de tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,9 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -45,8 +49,26 @@ public class TopicosController {
 		return TopicoDto.converter(topicos);
 	}
 	
+	@GetMapping("/por-nome")
+	public Page<TopicoDto> listarTopicoPorNomeCurso(@RequestParam(required = false) String nomeCurso, 
+			Integer page, Integer size) {
+		
+		Pageable pageable = PageRequest.of(page, size);
+		
+		Page<Topico> topicos = null;
+		
+		if (nomeCurso == null || nomeCurso.isBlank()) {
+			topicos = topicoRepository.findAll(pageable);
+		} else {
+			topicos = topicoRepository.findByCursoNome(nomeCurso, pageable);
+		}
+		
+		return TopicoDto.converterToPage(topicos);
+	}
+	
 	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, 
+			UriComponentsBuilder uriComponentsBuilder) {
 		
 		Topico topico = topicoForm.converter(cursoRepository);
 		
