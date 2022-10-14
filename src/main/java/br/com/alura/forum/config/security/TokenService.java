f658109baac3bb2117d669144d57299aef2503e9
mnishimori <mnishimori@yahoo.com.br>
2022-10-14 09:31:30 -0300

    Geração do token

---
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,45 @@
+package br.com.alura.forum.config.security;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${forum.jwt.expiration}")
+	public String expiration;
+
+	@Value("${forum.jwt.secret}")
+	public String secretKey;
+	
+	public String gerarToken(Authentication authenticate) {
+		
+		Usuario usuario = (Usuario) authenticate.getPrincipal();
+		
+		Date dataToken = Date.from(
+				LocalDateTime.now()
+				.atZone(ZoneId.systemDefault()).toInstant());
+		
+		Date dataTokenExpiration = Date.from(
+				LocalDateTime.now().plusMinutes(Long.parseLong(expiration))
+				.atZone(ZoneId.systemDefault()).toInstant());
+		
+		return Jwts.builder()
+				.setIssuer("API do fórum da Alura")
+				.setSubject(usuario.getId().toString())
+				.setIssuedAt(dataToken)
+				.setExpiration(dataTokenExpiration)
+				.signWith(SignatureAlgorithm.HS256, secretKey)
+				.compact();
+	}
+
+}
